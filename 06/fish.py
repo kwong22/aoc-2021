80ac5b613646393c88770848cb771756cf2bd76b
Kelvin Wong <kwong691@gmail.com>
2021-12-07 20:05:33 -0600

    Add day 6 part 1

---
+++ 06/fish.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+def read_lines_from_file(fname):
+    """Builds list of strings line-by-line from a file.
+
+    Args:
+        fname: name of file to read from
+    Returns:
+        List of strings
+    """
+    output = []
+    with open(fname, 'r') as lines:
+        for line in lines:
+            line = line.strip()
+            output.append(line)
+    return output
+
+def read_fish_from_line(line):
+    return [int(x) for x in line.split(',')]
+
+def simulate_fish(fish, num_days):
+    """Simulate fish
+
+    Args:
+        fish: List of integers representing number of days until reproduction
+    Returns:
+        Number of fish
+    """
+    temp = fish.copy()
+
+    for _ in range(num_days):
+        temp_add = []
+        for i in range(len(temp)):
+            if temp[i] > 0:
+                temp[i] -= 1
+            else:
+                temp[i] = 6
+                temp_add.append(8)
+        temp += temp_add
+
+    return len(temp)
+
+if __name__ == '__main__':
+    fish = read_fish_from_line(read_lines_from_file('input')[0])
+    print(simulate_fish(fish, 80))
